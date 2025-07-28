289ea7818a3812f31a6db129e4dfbdb77bfae1b3
Gabriel Jung <115058662+gabrieljungguimaraes@users.noreply.github.com>
2025-07-28 00:23:44 -0400

    Aula 01 - Contador While.c

---
+++ Aula 01 - Contador While.c
@@ -0,0 +1,15 @@
+int main()
+{
+    int contador = 10; //inicia o contador !
+    while (contador >= 1){ //usamos while para definir a condição !
+        printf("\n %d", contador ); // imprimi a variavel contador !
+        contador-- ; // repetição Contador -1 !
+    }
+
+    return 0;
+}
+
+//Resumo da aula:
+//while: repete enquanto a condição for verdadeira.
+//do...while: repete após executar uma vez, mesmo que a condição seja falsa depois.
+//for: usado para contagem definida (ex: de 1 a 10).
